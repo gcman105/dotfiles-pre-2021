cf0a6aaf9b8a5ab11a33faf7969054f595540377
gcman105 <gary@radi8.co.uk>
2020-01-15 23:24:08 +0100

    moved to git submodules

--- .emacs.d/snippets/markdown-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `markdown-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'markdown-mode
-		     '(("zzz" "gary@radi8.co.uk" "(my email)" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Thu Aug 21 12:05:35 2014
